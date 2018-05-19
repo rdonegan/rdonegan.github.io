14305a7c97fe8afd3698781167e756212b813527
Ryan Donegan <r.donegan44@gmail.com>
2018-05-19 17:37:24 -0400

    updated gifs for sugarwod and item selection

--- js/init.js
+++ js/init.js
@@ -1,110 +1,45 @@
 
-$(document).scroll(function() {
-
-    if ($(this).scrollTop() >= 50) {
-    	
-        // $('#logo-image').attr('src', 'img/Acme_Monogram_Colour.png')
-        $(".important-nav").addClass("padding-on-my-header");
-        
-        $(".name img").css("content","url('images/logo.png')");
-        $(".name img").css("max-width","50px");
-        $(".name img").css("margin","-.1em 0 0 0");
-
-    }
-    if ($(this).scrollTop() < 50) {
-
-        $(".important-nav").removeClass("padding-on-my-header");
-        // $(".name img").removeClass("image-small");
-        $(".name img").css("content","url('images/logo.png')");
-        $(".name img").css("max-width","77px");
-        $(".name img").css("margin","-1em 0 0 0");
-
-        
-    }
-});
 
 
 $(document).ready(function(){
 
-	$('.image-link').magnificPopup({
-		type:'image',
+	// portfolio tile controls
 
-
-	});
-
-	$('.popup-link').magnificPopup({ 
-	  	type: 'image',
-		closeOnContentClick: true
-	});
-
-
-	$(".proj_header").css('cursor', 'pointer');
-	$(".proj_header").hover(function(){
-		$(this).find(".dots").addClass('dots_hover');
-		$(this).find(".proj_image").addClass("hovered");
-
-	},
-		function(){
-			$(this).find(".dots").removeClass('dots_hover');
-			$(this).find(".proj_image").removeClass("hovered");
-		});
 	
-	$(".proj_header").click(function(){
-		
-		var sibling = $(this).next()
-		sibling.slideToggle(500);
-		$(this).find(".dots").toggleClass('dots_active');
-		
+	$('.item-selector-tile').mouseover(function(){
+		$(this).find('img').attr('src', 'images/portfolio/project-tiles/item-selector.gif');
 	});
 
-  $(".collapse_listen").css('cursor', 'pointer');
-  $(".collapse_listen").hover(function(){
-    // $(this).find(".dots").addClass('dots_hover');
-
-  },
-    function(){
-      // $(this).find(".dots").removeClass('dots_hover');
-    });
-  
-  $(".collapse_listen").click(function(){
-    
-    var parent = $(this).parent().parent().parent();
-    var oderp = parent.parent();
-    $('html, body').animate({
-        scrollTop: $(oderp).offset().top
-    }, 500);
-    //console.log(parent);
-    parent.slideToggle(500);
-    // parent.show("slide", {direction:"down"},500);
-    parent.prev().find(".dots").toggleClass('dots_active');
-    
-  });
-
-
-	$('nav a').on('click', function() {
-
-	    var scrollAnchor = $(this).attr('data-scroll')
-	    var scrollPoint  = $("#"+scrollAnchor).offset().top - 40;
-	                      
-	   $('body,html').animate({
-	       scrollTop: scrollPoint
-	   }, 500);
-	    
-	   return false;
-
+	$('.item-selector-tile').mouseout(function(){
+		$(this).find('img').attr('src', 'images/portfolio/project-tiles/item-selector.svg');
 	});
 
-	$(".proj_link").on('click', function(){
-
-		var scrollAnchor = $(this).attr('data-scroll')
-	    var scrollPoint  = $("#"+scrollAnchor).offset().top - 40;
-	                      
-	   $('body,html').animate({
-	       scrollTop: scrollPoint
-	   }, 500);
-	    
-	   return false;
-
+	$('.decision-tree-tile').mouseover(function(){
+		$(this).find('img').attr('src', 'images/portfolio/project-tiles/decision-tree.gif');
 	});
+
+	$('.decision-tree-tile').mouseout(function(){
+		$(this).find('img').attr('src', 'images/portfolio/project-tiles/decision-tree.svg');
+	});
+
+	$('.pricing-tile').mouseover(function(){
+		$(this).find('img').attr('src', 'images/portfolio/project-tiles/pricing.gif');
+	});
+
+	$('.pricing-tile').mouseout(function(){
+		$(this).find('img').attr('src', 'images/portfolio/project-tiles/pricing.svg');
+	});
+
+	$('.sugarwod-tile').mouseover(function(){
+		$(this).find('img').attr('src', 'images/portfolio/project-tiles/sugarwod.gif');
+	});
+
+	$('.sugarwod-tile').mouseout(function(){
+		$(this).find('img').attr('src', 'images/portfolio/project-tiles/sugarwodTile.svg');
+	});
+
+	
+
+
 
 });
