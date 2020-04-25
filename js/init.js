bacc11980e937e3e70a8f841b35ed340c95e119e
Ryan Donegan <r.donegan44@gmail.com>
2020-04-24 20:05:46 -0700

    added avatar

--- js/init.js
+++ js/init.js
@@ -3,6 +3,15 @@
 
 $(document).ready(function(){
 
+	var animation = bodymovin.loadAnimation({
+  container: document.getElementById('lottie'), // Required
+  path: 'data.json', // Required
+  renderer: 'svg/canvas/html', // Required
+  loop: true, // Optional
+  autoplay: true, // Optional
+  name: "Hello World", // Name for future reference. Optional.
+})
+
 	// portfolio tile controls
 
 	
